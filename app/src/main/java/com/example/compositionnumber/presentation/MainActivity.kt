b029425235ee8eccde49d95163047b72b206c5e5
Dmitry Chernenko <dmitrychernenko1995@gmail.com>
2023-10-11 21:58:00 +0300

    initial commit

---
+++ app/src/main/java/com/example/compositionnumber/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.compositionnumber.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.compositionnumber.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
