e1d2162348206e46e551c3c037417ab7a033d386
Dmitry Chernenko <dmitrychernenko1995@gmail.com>
2023-10-15 21:59:57 +0300

    create fragments classes and images and colors

---
+++ app/src/main/java/com/example/compositionnumber/presentation/WelcomeFragment.kt
@@ -0,0 +1,19 @@
+package com.example.compositionnumber.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.compositionnumber.R
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_welcome,container,false)
+    }
+}
